b3631413fa956e63762273943403dc74e72a5ee2
partth-somanni <parthsomani1787@gmail.com>
2026-06-14 23:02:46 +0530

    fix cors attempt no. 556

--- app/server/index.js
+++ app/server/index.js
@@ -7,12 +7,12 @@ const { Server } = require('socket.io')
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server, {
-  cors: { origin: 'http://localhost:5173', methods: ['GET', 'POST'] }
+  cors: { origin: '*', methods: ['GET', 'POST'] }
 })
 
 const db = new Database('docs.db')
 
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.json())
 
 db.exec(`
